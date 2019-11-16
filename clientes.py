607e99e5bc2ac75a99bdbdf8853a1ab13e67603c
vinicius.bonilha <vinicius.matos@totvs.com.br>
2019-11-15 23:28:14 -0300

    Persistindo dados no Mysql

--- clientes.py
+++ clientes.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
+
+app.secret_key = 'Fadergs'
 
 class Cliente:
     def __init__(self, nome, telefone, endereco, habilitado):
@@ -9,9 +11,20 @@ class Cliente:
         self.endereco = endereco
         self.habilitado = habilitado
 
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+usuario1 = Usuario('vinicius', 'vinicius bonilha', '1234')
+usuario2 = Usuario('juliana', 'Juliana Martins', '12345')
+
+usuarios = {usuario1.id: usuario1, usuario2: usuario2}
+
 vinicius = Cliente('Vinicius', '444','av terra nova', 'True')
-juliana = Cliente('Juliana', '444','av terra nova', 'True')
-ellen = Cliente('Ellen', '444','av terra nova', 'True')
+juliana = Cliente('Juliana', '555','av terra nova', 'True')
+ellen = Cliente('Ellen', '666','av terra nova', 'True')
 lista = [vinicius,juliana,ellen]
 
 @app.route('/')
@@ -20,6 +33,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Cliente')
 
 @app.route('/criar', methods=['POST'])
@@ -30,18 +45,31 @@ def criar():
     habilitado = request.form['habilitado']
     cliente = Cliente(nome, telefone, endereco, habilitado)
     lista.append(cliente)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima, )
 
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
-    if 'teste' == request.form['senha']:
-        return redirect('/')
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' Logado com sucesso')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
-        return redirect('/login')
+        flash('Senha errada')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado')
+    return redirect(url_for('login'))
 
 app.run(debug=True)
